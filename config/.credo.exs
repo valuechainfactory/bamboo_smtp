5b59458fa437da8a255b74d420ab2778374c054d
Kevin Disneur <kevin@disneur.me>
2018-06-28 19:25:22 +0200

    Bump Credo

--- config/.credo.exs
+++ config/.credo.exs
@@ -11,6 +11,7 @@
         {Credo.Check.Design.AliasUsage, false},
         # We can't keep it because it would force us to reformat version numbers (i.e: 20161023)
         # and to reformat our invoice amounts (i.e: 145_23)
+        {Credo.Check.Readability.ParenthesesOnZeroArityDefs, false},
         {Credo.Check.Readability.LargeNumbers, false},
         {Credo.Check.Readability.MaxLineLength, max_length: 120},
         {Credo.Check.Readability.ModuleDoc, false},
